b7d54248cb15244b0a9b45952d6c8135a104ad41
Nathanael White <narhanaelw99@gmail.com>
2021-07-13 22:41:55 -0500

    updating repo for askBCS

--- models/Pizza.js
+++ models/Pizza.js
@@ -35,13 +35,13 @@ const PizzaSchema = new Schema(
   }
 );
 
-// create the pizza model using the pizzaSchema
-const Pizza = model("Pizza", PizzaSchema);
 
 // get total count of comments and replies on retrieval
 PizzaSchema.virtual("commentCount").get(function () {
   return this.comments.length;
 });
+// create the pizza model using the pizzaSchema
+const Pizza = model("Pizza", PizzaSchema);
 
 // export the Pizza model
 module.exports = Pizza;
