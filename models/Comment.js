c2c6bb5f873c21b56e9bc35f1a6ae16e34763d3c
Nathanael White <narhanaelw99@gmail.com>
2021-07-18 14:24:18 -0500

    added validation

--- models/Comment.js
+++ models/Comment.js
@@ -9,10 +9,13 @@ const ReplySchema = new Schema(
         },
         // set custom id to avoid confusion with parent comment _id
         replyBody: {
-            type: String
+            type: String,
+            required: true,
+            trim: true
         },
         writtenBy: {
-            type: String
+            type: String,
+            required: true
         },
         createdAt: {
             type: Date,
@@ -29,10 +32,12 @@ const ReplySchema = new Schema(
 
 const CommentSchema = new Schema({
     writtenBy: {
-        type: String
+        type: String,
+        required: true
     },
     commentBody: {
-        type: String
+        type: String,
+        required: true
     },
     createdAt: {
         type: Date,
